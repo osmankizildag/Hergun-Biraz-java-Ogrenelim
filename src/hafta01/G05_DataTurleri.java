05d22ad464efe359f0307efc02a185d241214a67
osmankizildag <buchhalterkizildag@gmail.com>
2023-01-13 18:29:31 +0100

    130123.1828.Data Türlerinin Hazır Metodları

---
+++ src/hafta01/G05_DataTurleri.java
@@ -0,0 +1,19 @@
+package hafta01;
+
+public class G05_DataTurleri {
+    public static void main(String[] args) {
+
+        // Bazı data türlerinin hazır metodları vardır
+
+        String isim= "Nasreddin Hoca";
+
+        // örneğin tüm harfleri büyük/küçük yapabilirler.
+
+        System.out.println(isim.toUpperCase()); // NASREDDİN HOCA
+
+        System.out.println(isim.toLowerCase()); // nasreddin hoca
+
+        // non-primitive data turleri deger yaninda hazir method'lar da bulundururlar
+
+    }
+}
