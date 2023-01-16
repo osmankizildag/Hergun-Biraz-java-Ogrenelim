aede542e2b7a2e4a1d432f95427e958a88b070e3
Osman <buchhalterkizildag@gmail.com>
2023-01-16 18:21:18 +0100

    160123.1820.Kullanıcıdan Alınan Sayılarla Dört işlem Yapma

---
+++ src/hafta01/G07_DortIslemYapma.java
@@ -0,0 +1,30 @@
+package hafta01;
+
+import java.util.Scanner;
+
+public class G07_DortIslemYapma {
+    public static void main(String[] args) {
+
+        // Kullanicidan bir double, bir de int sayi isteyelim
+        // bunlarla dört işlem yapalım
+
+        Scanner sayi = new Scanner(System.in);
+
+        System.out.println("Lutfen ondalikli bir sayi giriniz ");
+
+        double ondalikliSayi= sayi.nextDouble();
+
+        System.out.println("Lutfen bir tamsayi giriniz");
+
+        int tamSayi= sayi.nextInt();
+
+        System.out.println("Girilen sayilarin toplami : " + (ondalikliSayi+tamSayi));
+
+        System.out.println("Girilen sayilarin carpimi : "+ (ondalikliSayi*tamSayi));
+
+        System.out.println("Girilen sayilarin farkı : "+ (ondalikliSayi-tamSayi));
+
+        System.out.println("Girilen sayilarin bölümü : "+ (ondalikliSayi/tamSayi));
+
+    }
+}
